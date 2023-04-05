722688dca811be9f4e57ad6979100aeced8a7833
Eric Seidel <eric@shorebird.dev>
2023-04-05 16:17:49 -0700

    Make build.sh work locally
    build.sh wasn't tested before this, but I've now validated that it works locally with an (optional) 3rd arg which points to the engine location (instead of /engine).
    
    This unfortunately also includes a change that won't work inside docker of depending on an NDK outside of Flutter's since (at least my copy) of the NDK Flutter engine pulled down does not seem to include all of the necessary arm64 dependencies.
    
    https://github.com/flutter/flutter/issues/124280

--- build_engine/build.sh
+++ build_engine/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
 
 # Usage:
-# ./build_engine git_hash
+# ./build_engine git_hash [engine_path]
 
 ENGINE_HASH=$1
 
 # The path to the Flutter engine.
-ENGINE_ROOT=/engine
+ENGINE_ROOT="${2:-/engine}"
 ENGINE_SRC=$ENGINE_ROOT/src
 ENGINE_OUT=$ENGINE_SRC/out
 UPDATER_SRC=$ENGINE_SRC/third_party/updater
@@ -24,29 +24,31 @@ gclient sync --revision src/flutter@$ENGINE_HASH
 cd $UPDATER_SRC/library
 
 # Use the same NDK as the Flutter engine.
-ANDROID_NDK_HOME="/engine/src/third_party/android_tools/ndk"
 
-# Build both the arm64 and armv7 versions of the library.
-# Use -p 16 to match Flutter's minAndroidSdkVersion
-cargo ndk \
-    -p 16 \
-    --target=aarch64-linux-android \
-    --target=armv7-linux-androideabi \
-    # --target=i686-linux-android \
-    # --target=x86_64-linux-android \
-    build --release
 
 # FIXME: This should be part of the gn/ninja build process!
 # I haven't investigated how to build rust from GN with the Android NDK yet.
+
+# Build both the arm64 and armv7 versions of the library.
+# Use -p 16 to match Flutter's minAndroidSdkVersion
+NDK_HOME="$ENGINE_SRC/third_party/android_tools/ndk" cargo ndk -p 16 -t armeabi-v7a build --release
+
+# However aarch64-linux-android16-clang only starts at v21, unsure which
+# the Flutter engine uses, using most recent for now?
+# Unfortuantely Flutter's NDK doesn't seem to include libunwind?
+# https://github.com/flutter/flutter/issues/124280
+# FIXME: Using Android Studio NDK for now, this won't work on the bots!
+cargo ndk -t arm64-v8a build --release
+
 # Patch the engine with the updater library
 # These match paths set in flutter/shell/platform/android/BUILD.gn
-UPDATER_OUT=/engine/src/flutter/updater
+UPDATER_OUT=$ENGINE_SRC/flutter/updater
 mkdir -p $UPDATER_OUT
 cp $UPDATER_SRC/library/include/updater.h $UPDATER_OUT
 mkdir -p $UPDATER_OUT/android_aarch64
-COPY $UPDATER_SRC/target/aarch64-linux-android/release/libupdater.a $UPDATER_OUT/android_aarch64
+cp $UPDATER_SRC/target/aarch64-linux-android/release/libupdater.a $UPDATER_OUT/android_aarch64
 mkdir -p $UPDATER_OUT/android_arm
-COPY $UPDATER_SRC/target/armv7-linux-androideabi/release/libupdater.a $UPDATER_OUT/android_arm
+cp $UPDATER_SRC/target/armv7-linux-androideabi/release/libupdater.a $UPDATER_OUT/android_arm
 
 # Build the patch tool.
 # Again, this belongs as part of the gn build.
@@ -67,8 +69,8 @@ ninja -C ./out/android_release
 
 # Copy Shorebird engine artifacts to Google Cloud Storage.
 # Only need the libflutter.so (and flutter.jar) artifacts
-RUN gsutil cp $ENGINE_OUT/android_release_arm64/zip_archives/artifact.zip gs://download.shorebird.dev/$ENGINE_HASH/
-RUN gsutil cp $ENGINE_OUT/android_release_arm64/zip_archives/symbols.zip gs://download.shorebird.dev/$ENGINE_HASH/
+gsutil cp $ENGINE_OUT/android_release_arm64/zip_archives/artifact.zip gs://download.shorebird.dev/$ENGINE_HASH/
+gsutil cp $ENGINE_OUT/android_release_arm64/zip_archives/symbols.zip gs://download.shorebird.dev/$ENGINE_HASH/
 
-RUN gsutil cp $ENGINE_OUT/android_release/zip_archives/artifact.zip gs://download.shorebird.dev/$ENGINE_HASH/
-RUN gsutil cp $ENGINE_OUT/android_release/zip_archives/symbols.zip gs://download.shorebird.dev/$ENGINE_HASH/
+gsutil cp $ENGINE_OUT/android_release/zip_archives/artifact.zip gs://download.shorebird.dev/$ENGINE_HASH/
+gsutil cp $ENGINE_OUT/android_release/zip_archives/symbols.zip gs://download.shorebird.dev/$ENGINE_HASH/
