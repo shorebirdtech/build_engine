9bb67edf6b9a02f4b05dd9ec8c2349d72b917425
Eric Seidel <eric@shorebird.dev>
2023-04-05 18:32:12 -0700

    Further simplify and fix MAVEN_PATH error for armv7

--- build_engine/build.sh
+++ build_engine/build.sh
@@ -79,10 +79,9 @@ MAVEN_ROOT="gs://download.shorebird.dev/download.flutter.io/io/flutter/$MAVEN_VE
 # Android Arm64 release Flutter artifacts
 ARCH_OUT=$ENGINE_OUT/android_release_arm64
 ZIPS_OUT=$ARCH_OUT/zip_archives/android-arm64-release
-gsutil cp $ZIPS_OUT/artifacts.zip \
-    $INFRA_ROOT/android-arm64-release/artifacts.zip
-gsutil cp $ZIPS_OUT/symbols.zip \
-    $INFRA_ROOT/android-arm64-release/symbols.zip
+ZIPS_DEST=$INFRA_ROOT/android-arm64-release
+gsutil cp $ZIPS_OUT/artifacts.zip $ZIPS_DEST/artifacts.zip
+gsutil cp $ZIPS_OUT/symbols.zip $ZIPS_DEST/symbols.zip
 # Android Arm64 release Maven artifacts
 ARCH_PATH=$ARCH_OUT/arm64_v8a_release
 MAVEN_PATH=$MAVEN_ROOT/android-arm64-release/arm64_v8a_release-$MAVEN_VER
@@ -93,13 +92,12 @@ gsutil cp $ARCH_PATH.maven-metadata.xml $MAVEN_PATH.maven-metadata.xml
 # Android Arm32 release Flutter artifacts
 ARCH_OUT=$ENGINE_OUT/android_release
 ZIPS_OUT=$ARCH_OUT/zip_archives/android-arm-release
-gsutil cp $ZIPS_OUT/artifacts.zip \
-    $INFRA_ROOT/android-arm-release/artifacts.zip
-gsutil cp $ZIPS_OUT/symbols.zip \
-    $INFRA_ROOT/android-arm-release/symbols.zip
+ZIPS_DEST=$INFRA_ROOT/android-arm-release
+gsutil cp $ZIPS_OUT/artifacts.zip $ZIPS_DEST/artifacts.zip
+gsutil cp $ZIPS_OUT/symbols.zip $ZIPS_DEST/symbols.zip
 # Android Arm32 release Maven artifacts
 ARCH_PATH=$ARCH_OUT/armeabi_v7a_release
-MAVEN_PATH=android-arm-release/armeabi_v7a_release-$MAVEN_VER
+MAVEN_PATH=$MAVEN_ROOT/android-arm-release/armeabi_v7a_release-$MAVEN_VER
 gsutil cp $ARCH_PATH.pom $MAVEN_PATH.pom
 gsutil cp $ARCH_PATH.jar $MAVEN_PATH.jar
 gsutil cp $ARCH_PATH.maven-metadata.xml $MAVEN_PATH.maven-metadata.xml
@@ -107,7 +105,8 @@ gsutil cp $ARCH_PATH.maven-metadata.xml $MAVEN_PATH.maven-metadata.xml
 # Not sure which flutter_embedding_release files to use? 32 or 64 bit?
 # It does not seem to contain the libflutter.so file, but does seem to
 # differ between the two build dirs.
-ARCH_PATH=$ENGINE_OUT/android_release/flutter_embedding_release
+ARCH_OUT=$ENGINE_OUT/android_release
+ARCH_PATH=$ARCH_OUT/flutter_embedding_release
 MAVEN_PATH=$MAVEN_ROOT/android-arm-release/flutter_embedding_release-$MAVEN_VER
 gsutil cp $ARCH_PATH.pom $MAVEN_PATH.pom
 gsutil cp $ARCH_PATH.jar $MAVEN_PATH.jar
