823f461546660d15ed9211b4d9b2708347458acf
Eric Seidel <eric@shorebird.dev>
2023-04-05 18:44:47 -0700

    Split out upload.sh and fix MAVEN_PATH
    I still got the MAVEN_PATH wrong, fixed.
    
    Also split out upload.sh from build.sh for faster iteration.

--- build_engine/build.sh
+++ build_engine/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Usage:
-# ./build_engine git_hash [engine_path]
+# ./build.sh git_hash [engine_path]
 
 ENGINE_HASH=$1
 
@@ -68,46 +68,4 @@ ninja -C ./out/android_release_arm64
 ninja -C ./out/android_release
 
 # Copy Shorebird engine artifacts to Google Cloud Storage.
-# FIXME: This should not be in shell, it's too complicated/repetative.
-# Only need the libflutter.so (and flutter.jar) artifacts
-# Artifact list: https://github.com/shorebirdtech/shorebird/pull/222/commits/a1fbbf7b93029b90ebd79c9ffeaafd3ee475cf20
-
-INFRA_ROOT="gs://download.shorebird.dev/flutter_infra_release/flutter/$ENGINE_HASH"
-MAVEN_VER="1.0.0-$ENGINE_HASH"
-MAVEN_ROOT="gs://download.shorebird.dev/download.flutter.io/io/flutter/$MAVEN_VER"
-
-# Android Arm64 release Flutter artifacts
-ARCH_OUT=$ENGINE_OUT/android_release_arm64
-ZIPS_OUT=$ARCH_OUT/zip_archives/android-arm64-release
-ZIPS_DEST=$INFRA_ROOT/android-arm64-release
-gsutil cp $ZIPS_OUT/artifacts.zip $ZIPS_DEST/artifacts.zip
-gsutil cp $ZIPS_OUT/symbols.zip $ZIPS_DEST/symbols.zip
-# Android Arm64 release Maven artifacts
-ARCH_PATH=$ARCH_OUT/arm64_v8a_release
-MAVEN_PATH=$MAVEN_ROOT/android-arm64-release/arm64_v8a_release-$MAVEN_VER
-gsutil cp $ARCH_PATH.pom $MAVEN_PATH.pom
-gsutil cp $ARCH_PATH.jar $MAVEN_PATH.jar
-gsutil cp $ARCH_PATH.maven-metadata.xml $MAVEN_PATH.maven-metadata.xml
-
-# Android Arm32 release Flutter artifacts
-ARCH_OUT=$ENGINE_OUT/android_release
-ZIPS_OUT=$ARCH_OUT/zip_archives/android-arm-release
-ZIPS_DEST=$INFRA_ROOT/android-arm-release
-gsutil cp $ZIPS_OUT/artifacts.zip $ZIPS_DEST/artifacts.zip
-gsutil cp $ZIPS_OUT/symbols.zip $ZIPS_DEST/symbols.zip
-# Android Arm32 release Maven artifacts
-ARCH_PATH=$ARCH_OUT/armeabi_v7a_release
-MAVEN_PATH=$MAVEN_ROOT/android-arm-release/armeabi_v7a_release-$MAVEN_VER
-gsutil cp $ARCH_PATH.pom $MAVEN_PATH.pom
-gsutil cp $ARCH_PATH.jar $MAVEN_PATH.jar
-gsutil cp $ARCH_PATH.maven-metadata.xml $MAVEN_PATH.maven-metadata.xml
-
-# Not sure which flutter_embedding_release files to use? 32 or 64 bit?
-# It does not seem to contain the libflutter.so file, but does seem to
-# differ between the two build dirs.
-ARCH_OUT=$ENGINE_OUT/android_release
-ARCH_PATH=$ARCH_OUT/flutter_embedding_release
-MAVEN_PATH=$MAVEN_ROOT/android-arm-release/flutter_embedding_release-$MAVEN_VER
-gsutil cp $ARCH_PATH.pom $MAVEN_PATH.pom
-gsutil cp $ARCH_PATH.jar $MAVEN_PATH.jar
-gsutil cp $ARCH_PATH.maven-metadata.xml $MAVEN_PATH.maven-metadata.xml
+./upload.sh $ENGINE_HASH $ENGINE_ROOT
---
+++ build_engine/upload.sh
@@ -0,0 +1,53 @@
+#!/bin/sh -e
+
+# Usage:
+# ./upload.sh git_hash engine_path
+ENGINE_HASH=$1
+ENGINE_ROOT=$2
+
+ENGINE_SRC=$ENGINE_ROOT/src
+ENGINE_OUT=$ENGINE_SRC/out
+
+# FIXME: This should not be in shell, it's too complicated/repetative.
+# Only need the libflutter.so (and flutter.jar) artifacts
+# Artifact list: https://github.com/shorebirdtech/shorebird/pull/222/commits/a1fbbf7b93029b90ebd79c9ffeaafd3ee475cf20
+
+INFRA_ROOT="gs://download.shorebird.dev/flutter_infra_release/flutter/$ENGINE_HASH"
+MAVEN_VER="1.0.0-$ENGINE_HASH"
+MAVEN_ROOT="gs://download.shorebird.dev/download.flutter.io/io/flutter"
+
+# Android Arm64 release Flutter artifacts
+ARCH_OUT=$ENGINE_OUT/android_release_arm64
+ZIPS_OUT=$ARCH_OUT/zip_archives/android-arm64-release
+ZIPS_DEST=$INFRA_ROOT/android-arm64-release
+gsutil cp $ZIPS_OUT/artifacts.zip $ZIPS_DEST/artifacts.zip
+gsutil cp $ZIPS_OUT/symbols.zip $ZIPS_DEST/symbols.zip
+# Android Arm64 release Maven artifacts
+ARCH_PATH=$ARCH_OUT/arm64_v8a_release
+MAVEN_PATH=$MAVEN_ROOT/arm64_v8a_release/$MAVEN_VER/arm64_v8a_release-$MAVEN_VER
+gsutil cp $ARCH_PATH.pom $MAVEN_PATH.pom
+gsutil cp $ARCH_PATH.jar $MAVEN_PATH.jar
+gsutil cp $ARCH_PATH.maven-metadata.xml $MAVEN_PATH.maven-metadata.xml
+
+# Android Arm32 release Flutter artifacts
+ARCH_OUT=$ENGINE_OUT/android_release
+ZIPS_OUT=$ARCH_OUT/zip_archives/android-arm-release
+ZIPS_DEST=$INFRA_ROOT/android-arm-release
+gsutil cp $ZIPS_OUT/artifacts.zip $ZIPS_DEST/artifacts.zip
+gsutil cp $ZIPS_OUT/symbols.zip $ZIPS_DEST/symbols.zip
+# Android Arm32 release Maven artifacts
+ARCH_PATH=$ARCH_OUT/armeabi_v7a_release
+MAVEN_PATH=$MAVEN_ROOT/armeabi_v7a_release/$MAVEN_VER/armeabi_v7a_release-$MAVEN_VER
+gsutil cp $ARCH_PATH.pom $MAVEN_PATH.pom
+gsutil cp $ARCH_PATH.jar $MAVEN_PATH.jar
+gsutil cp $ARCH_PATH.maven-metadata.xml $MAVEN_PATH.maven-metadata.xml
+
+# Not sure which flutter_embedding_release files to use? 32 or 64 bit?
+# It does not seem to contain the libflutter.so file, but does seem to
+# differ between the two build dirs.
+ARCH_OUT=$ENGINE_OUT/android_release
+ARCH_PATH=$ARCH_OUT/flutter_embedding_release
+MAVEN_PATH=$MAVEN_ROOT/flutter_embedding_release/$MAVEN_VER/flutter_embedding_release-$MAVEN_VER
+gsutil cp $ARCH_PATH.pom $MAVEN_PATH.pom
+gsutil cp $ARCH_PATH.jar $MAVEN_PATH.jar
+gsutil cp $ARCH_PATH.maven-metadata.xml $MAVEN_PATH.maven-metadata.xml
