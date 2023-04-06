221d1fcd7bcc9fd9884bd467377bea23a8569499
Eric Seidel <eric@shorebird.dev>
2023-04-05 21:12:16 -0700

    Update build_and_upload.sh
    Fix to cd into the script dir before trying to find build.sh.

--- build_engine/build_and_upload.sh
+++ build_engine/build_and_upload.sh
@@ -7,12 +7,16 @@
 ENGINE_ROOT=$1
 ENGINE_HASH=$2
 
+SCRIPT_DIR=$(dirname "$0")
+
 echo "Building engine at $ENGINE_ROOT and uploading to gs://download.shorebird.dev"
 
 # First update our checkouts to the correct versions.
 cd $ENGINE_ROOT
 gclient sync --revision src/flutter@$ENGINE_HASH
 
+
+cd $SCRIPT_DIR
 # Then run the build (this should just be a ninja call).
 ./build.sh $ENGINE_ROOT
 
