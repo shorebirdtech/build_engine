6552ffbdaf698d65b21cea56a19aabdf5e1cf48e
Eric Seidel <eric@shorebird.dev>
2023-06-12 10:54:33 -0400

    feat: support iOS builds (#8)

--- build_engine/upload.sh
+++ build_engine/upload.sh
@@ -102,3 +102,13 @@ gsutil cp patch-x86_64-pc-windows-msvc.zip $SHOREBIRD_ROOT/patch-windows-x64.zip
 gsutil cp patch-x86_64-unknown-linux-gnu.zip $SHOREBIRD_ROOT/patch-linux-x64.zip
 
 gsutil cp $MANIFEST_FILE $SHOREBIRD_ROOT/artifacts_manifest.yaml
+
+
+# Match the upload pattern from iOS:
+# https://github.com/flutter/engine/commit/1d7f0c66c316a37105601b13136f890f6595aebc
+
+# iOS release Flutter artifacts
+ARCH_OUT=$ENGINE_OUT/release
+ZIPS_OUT=$ARCH_OUT
+ZIPS_DEST=$INFRA_ROOT/ios-release
+gsutil cp $ZIPS_OUT/artifacts.zip $ZIPS_DEST/artifacts.zip
